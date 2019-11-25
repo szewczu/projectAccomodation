5e92f9d85c057fa32dd64e0b7ddc89438afcbe4b
Wilk <skrok.jan@gmail.com>
2019-11-25 22:28:03 +0100

    Deleted ManageAccount and Migrations

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -10,6 +10,8 @@ namespace Noclegi.Areas.Identity.Pages.Account.Manage
     {
         public static string Index => "Index";
 
+        public static string Email => "Email";
+
         public static string ChangePassword => "ChangePassword";
 
         public static string ExternalLogins => "ExternalLogins";
@@ -19,6 +21,8 @@ namespace Noclegi.Areas.Identity.Pages.Account.Manage
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
