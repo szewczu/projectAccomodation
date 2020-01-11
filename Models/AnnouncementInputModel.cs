549b772c732e75fbcd02c989ca7c51afea8b5063
Kamil Szewczyk <szewczu11@gmail.com>
2020-01-11 22:00:14 +0100

    final version with new exchange script

--- Models/AnnouncementInputModel.cs
+++ Models/AnnouncementInputModel.cs
@@ -9,37 +9,40 @@ namespace Noclegi.Areas.Announcement.Pages
     {
         public class AnnouncementInputModel
         {
-            [Required]
+            [Required(ErrorMessage = "Wprowadź tytuł ogłoszenia")]
             [Display(Name = "Tytul")]
             public string Title { get; set; }
 
             [Display(Name = "Opis")]
             public string Description { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Wprowadź datę rozpoczęcia")]
             [DataType(DataType.Date)]
             [Display(Name = "Data Rozpoczecia")]
             public DateTime StartDate { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Wprowadź datę zakończenia")]
             [Display(Name = "Data Zakonczenia")]
             [DataType(DataType.Date)]
             public DateTime EndDate { get; set; }
 
+            [Required(ErrorMessage = "Wprowadź cenę")]
             [Display(Name = "Cena")]
             public int Price { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Wprowadź typ ogłoszenia")]
             [Display(Name = "Typ Ogloszenia")]
             public string TypeOfAdvertisement { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Wprowadź rodzaj ogłoszenia")]
             [Display(Name = "Property Type")]
             public string PropertyType { get; set; }
 
+            [Required(ErrorMessage = "Wprowadź numer piętra")]
             [Display(Name = "Pietro")]
             public int Floor { get; set; }
 
+            [Required(ErrorMessage = "Wprowadź liczbę pokoi")]
             [Display(Name = "Pokoje")]
             public int Rooms { get; set; }
 
